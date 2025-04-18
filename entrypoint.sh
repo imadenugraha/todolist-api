8cb3d76e0d75dcedd26325978f66611eed0b15ce
imadenugraha <imadenugraha@gmail.com>
2025-04-18 19:56:40 +0700

    Add config and view cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,10 @@
 set -xe
 
 echo "📦 Running migration"
-cd /var/www && php artisan migrate --force
+cd /var/www && \
+    php artisan config:cache && \
+    php artisan view:cache && \
+    php artisan migrate --force
 
 echo "🚀 Running Server"
-cd /var/www && exec php artisan octane:frankenphp --workers=16 --host=0.0.0.0 --port=9804
+cd /var/www && exec php artisan octane:frankenphp --workers=4 --host=0.0.0.0 --port=9804
