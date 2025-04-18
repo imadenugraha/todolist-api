8950224ef12dcb0492eca2bae225d675f713b29a
imadenugraha <imadenugraha@gmail.com>
2025-04-18 16:16:21 +0700

    Change directory to /var/www

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Running migration"
-php artisan migrate --force
+echo "📦 Running migration"
+cd /var/www && php artisan migrate --force
 
-echo "Running Server"
-exec php artisan octane:frankenphp --workers=16 --host=0.0.0.0 --port=9804 --watch
+echo "🚀 Running Server"
+exec cd /var/www && php artisan octane:frankenphp --workers=16 --host=0.0.0.0 --port=9804 --watch
