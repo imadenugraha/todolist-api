98424e81d19e3afae5e0b02cb14a99b81fccbf6d
imadenugraha <imadenugraha@gmail.com>
2025-04-18 15:56:55 +0700

    Add exec for running server

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "Running migration"
 php artisan migrate --force
 
 echo "Running Server"
-php artisan octane:frankenphp --workers=16 --host=0.0.0.0 --port=9804 --watch
+exec php artisan octane:frankenphp --workers=16 --host=0.0.0.0 --port=9804 --watch
