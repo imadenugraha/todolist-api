63eceb8bfa7ba7b7f2c1ae2dbe6118760a9d55a1
imadenugraha <imadenugraha@gmail.com>
2025-04-18 17:01:50 +0700

    Fix error exec cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "📦 Running migration"
 cd /var/www && php artisan migrate --force
 
 echo "🚀 Running Server"
-exec cd /var/www && php artisan octane:frankenphp --workers=16 --host=0.0.0.0 --port=9804 --watch
+cd /var/www && exec php artisan octane:frankenphp --workers=16 --host=0.0.0.0 --port=9804 --watch
